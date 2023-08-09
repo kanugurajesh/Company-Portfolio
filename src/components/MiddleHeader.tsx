cad15575d55b6a4d159a1b37c942a05e3ae727fc
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-10 00:12:29 +0530

    add

--- src/components/MiddleHeader.tsx
+++ src/components/MiddleHeader.tsx
@@ -2,7 +2,7 @@ import "../css/MiddleHeader.css";
 
 function App() {
     return (
-        <section id="section1">
+        <section id="section1" style={{color:'white'}}>
             <h1>The Cosmos <span>Data</span> & <span>Development</span> Platform</h1>
         </section>
     )
