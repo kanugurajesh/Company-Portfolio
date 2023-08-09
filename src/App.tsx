af3ab7bfaad3b1190838aaaffc99d6d761da77be
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-09 20:54:52 +0530

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import TopHeader from './components/TopHeader'
+import LowScreen from './components/LowScreen'
 
 function App() {
   return (
     <>
       <TopHeader />
+      {/* <LowScreen /> */}
     </>
   )
 }
---
+++ src/components/LowScreen.tsx
@@ -0,0 +1,18 @@
+function App() {
+    return (
+      <div id="header">
+        <div id="top-header">
+            <div className="logo"><img src="/rajesh.png" alt="" /> Rajesh</div>
+            <div id="slide-docs"></div>
+            <div id="menu"></div>
+        </div>
+        <div id="down-header">
+            <h3>Home</h3>
+            <h3>Contact</h3>
+        </div>
+      </div>
+    )
+  }
+  
+  export default App
+  
