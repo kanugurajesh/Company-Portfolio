e8a3d27d11845c0bac7bf3f5e5a56aa60afe1371
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-09 19:45:14 +0530

    adding

---
+++ src/components/TopHeader.tsx
@@ -0,0 +1,12 @@
+import "../css/TopHeader.css";
+
+function App() {
+    return (
+      <>
+        <h4>We just launched Lenses - An advanced analytics dashboard for Osmosis - VISIT IT NOW → <a href="">datalenses.zone</a></h4>
+      </>
+    )
+  }
+  
+  export default App
+  
