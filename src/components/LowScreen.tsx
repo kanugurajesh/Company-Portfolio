08a001241803f1b93717cf117e14b907bf9f76e1
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-09 23:03:31 +0530

    add

--- src/components/LowScreen.tsx
+++ src/components/LowScreen.tsx
@@ -1,25 +1,49 @@
-import "../css/LowScreen.css"
+import React, { useState, useEffect } from 'react';
+import '../css/LowScreen.css';
 
 function App() {
-    return (
-      <div id="header">
-        <div id="top-header">
-            <div id="logo"><img src="/vect.svg" alt="" /> <span>Gmate</span></div>
-            <div>
-              <div id="menu">
-                <div></div>
-                <div></div>
-                <div></div>
-              </div>
-            </div>
+  const [showDocs, setShowDocs] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 478) {
+        setShowDocs(false);
+      } else {
+        setShowDocs(true);
+      }
+    };
+
+    handleResize(); // Call the function once to set the initial state
+
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <div id="header">
+      <div id="top-header">
+        <div id="logo">
+          <img src="/vect.svg" alt="" /> <span>Gmate</span>
         </div>
-        <div id="down-header">
-            <h3>Home</h3>
-            <h3>Contact</h3>
+        <div id='bar'>
+          {showDocs && <div id="docs">Docs <span>→</span></div>}
+          <div id="menu">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
       </div>
-    )
-  }
-  
-  export default App
-  
+      <div id="down-header">
+        <h3>Home</h3>
+        <h3>Contact</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
