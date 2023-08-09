e4588f51d6512f86d4f7f528853a7c631ee9373c
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-09 22:35:25 +0530

    add

--- src/components/LowScreen.tsx
+++ src/components/LowScreen.tsx
@@ -5,10 +5,12 @@ function App() {
       <div id="header">
         <div id="top-header">
             <div id="logo"><img src="/vect.svg" alt="" /> <span>Gmate</span></div>
-            <div id="menu">
-              <div></div>
-              <div></div>
-              <div></div>
+            <div>
+              <div id="menu">
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
             </div>
         </div>
         <div id="down-header">
