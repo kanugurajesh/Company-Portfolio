cebfd9f4a68a8ded35dfcd5e50616e105efb5ddf
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-10 00:10:29 +0530

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import TopHeader from './components/TopHeader'
 import LowScreen from './components/LowScreen'
+import MiddleHeader from './components/MiddleHeader'
 
 function App() {
   return (
     <>
       <TopHeader />
       <LowScreen />
+      <MiddleHeader />
     </>
   )
 }
---
+++ src/components/MiddleHeader.tsx
@@ -0,0 +1,12 @@
+import "../css/MiddleHeader.css";
+
+function App() {
+    return (
+        <section id="section1">
+            <h1>The Cosmos <span>Data</span> & <span>Development</span> Platform</h1>
+        </section>
+    )
+  }
+  
+export default App
+  
