1ac9b4628e2c7c740b9180d07dcf9c12848715e6
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-10 23:47:19 +0530

    add

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,15 +6,15 @@ function App() {
             <h1>Products</h1>
             <div id="boxes">
                 <div className="box">
-                    <h1>SQL Access</h1>
+                    <h2>SQL Access</h2>
                     <img src="/sql.png" alt="" />
                 </div>
                 <div className="box">
-                    <h1>RPC Access</h1>
+                    <h2>RPC Access</h2>
                     <img src="/rpc.png" alt="" />
                 </div>
                 <div className="box">
-                    <h1>Advanced API</h1>
+                    <h2>Advanced API</h2>
                     <img src="/adapi.png" alt="" />
                 </div>
             </div>
