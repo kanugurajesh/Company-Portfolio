8f78f8dd79d970c1e4ee9bdf364eb97abdda41eb
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-11 23:55:36 +0530

    add

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,21 +3,23 @@ import "../css/Footer.css"
 function App() {
     return (
         <section id="footer">
-            <div>
-                <h3 style={{marginBottom:'2rem'}}>Navigation</h3>
-                <h3 style={{color:'#b2d1f5'}}>Home</h3>
-                <h3 style={{color:'#b2d1f5'}}>Contact</h3>
-            </div>
-            <div>
-                <h3>Resources</h3>
-                <h3 style={{color:'#b2d1f5'}}>Docs</h3>
-            </div>
-            <div>
-                <h3>Social</h3>
-                <ul style={{listStyle:'none'}}>
-                    <li style={{color:'#b2d1f5'}}><a href="https://twitter.com/exploringengin1"><img src="/twitter.svg" alt="" /></a></li>
-                    <li style={{color:'#b2d1f5'}}><a href="https://github.com/rajesh604"><img src="/github.svg" alt="" /></a></li>
-                </ul>
+            <div id="footer-div">
+                <div>
+                    <h3 style={{marginBottom:'2rem'}}>Navigation</h3>
+                    <h3 style={{color:'#b2d1f5'}}>Home</h3>
+                    <h3 style={{color:'#b2d1f5'}}>Contact</h3>
+                </div>
+                <div>
+                    <h3>Resources</h3>
+                    <h3 style={{color:'#b2d1f5'}}>Docs</h3>
+                </div>
+                <div>
+                    <h3>Social</h3>
+                    <ul style={{listStyle:'none'}}>
+                        <li style={{color:'#b2d1f5'}}><a href="https://twitter.com/exploringengin1"><img src="/twitter.svg" alt="" /></a></li>
+                        <li style={{color:'#b2d1f5'}}><a href="https://github.com/rajesh604"><img src="/github.svg" alt="" /></a></li>
+                    </ul>
+                </div>
             </div>
             <footer>
                 <div>
