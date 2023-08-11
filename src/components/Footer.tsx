885a9e5b98e78725308bb30e6f3bed39c32fcc52
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-11 22:29:43 +0530

    add

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,24 @@ import "../css/Footer.css"
 function App() {
     return (
         <>
-            
+            <section id="footer">
+                <div>
+                    <h3 style={{marginBottom:'3rem'}}>Navigation</h3>
+                    <h3>Home</h3>
+                    <h3>Contact</h3>
+                </div>
+                <div>
+                    <h1>Resources</h1>
+                    <h1>Docs</h1>
+                </div>
+                <div>
+                    <h1>Social</h1>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </section> 
         </>
     )
 }
