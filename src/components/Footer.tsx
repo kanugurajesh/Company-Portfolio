8013c15db3f3fe7e88aae6c49b23211d2b891c0e
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-11 23:01:09 +0530

    add

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,18 +5,18 @@ function App() {
         <section id="footer">
             <div>
                 <h3 style={{marginBottom:'3rem'}}>Navigation</h3>
-                <h3>Home</h3>
-                <h3>Contact</h3>
+                <h3 style={{color:'#b2d1f5'}}>Home</h3>
+                <h3 style={{color:'#b2d1f5'}}>Contact</h3>
             </div>
             <div>
                 <h3>Resources</h3>
-                <h3>Docs</h3>
+                <h3 style={{color:'#b2d1f5'}}>Docs</h3>
             </div>
             <div>
                 <h3>Social</h3>
                 <ul style={{listStyle:'none'}}>
-                    <li><a href="https://twitter.com/exploringengin1"><img src="/twitter.svg" alt="" /></a></li>
-                    <li><a href="https://github.com/rajesh604"><img src="/github.svg" alt="" /></a></li>
+                    <li style={{color:'#b2d1f5'}}><a href="https://twitter.com/exploringengin1"><img src="/twitter.svg" alt="" /></a></li>
+                    <li style={{color:'#b2d1f5'}}><a href="https://github.com/rajesh604"><img src="/github.svg" alt="" /></a></li>
                 </ul>
             </div>
         </section> 
