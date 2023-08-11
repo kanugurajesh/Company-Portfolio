e9db521174ab731ccbeb05f8445058a9474855f3
Rajesh604 <kanugurajesh3@gmail.com>
2023-08-11 19:18:31 +0530

    add

--- src/components/Build.tsx
+++ src/components/Build.tsx
@@ -2,43 +2,45 @@ import "../css/Build.css"
 
 function App() {
     return (
-        <div id="build">
-            <h2>Built for all types of projects</h2>
-            <div>
-                <div className="block">
-                    <h3>App Chains</h3>
-                    <p>Extend your chain functionality with powerful Analytics and reliable APIs.</p>
-                </div>
-                <div className="block">
-                    <h3>Wallets</h3>
-                    <p>Forget about managing your own infrastructure and focus on building a great wallet experience.</p>
-                </div>
-                <div className="block">
-                    <h3>Validators</h3>
-                    <p>Measure delegator loyalty and build tools to engage with them.</p>
-                </div>
-                <div className="block">
-                    <h3>Analysts</h3>
-                    <p>Dive deep into Cosmos on-chain data and run complex analysis.</p>
-                </div>
-                <div className="block">
-                    <h3>MEV</h3>
-                    <p>Access on-chain data and necessary infrastructure.</p>
-                </div>
-                <div className="block">
-                    <h3>Smart Contracts</h3>
-                    <p>Obtain on-chain data and get custom endpoints for your smart contract.</p>
-                </div>
-                <div className="block">
-                    <h3>Defi Apps</h3>
-                    <p>Forget about complex RPC queries and request all data from one single platform.</p>
-                </div>
-                <div className="block">
-                    <h3>IBC Apps</h3>
-                    <p>The most comprehensive data set for IBC chains and scalable IBC infrastructure.</p>
+        <section id="build">
+            <div id="build-inner">
+                <h2>Built for all types of projects</h2>
+                <div>
+                    <div className="block">
+                        <h3>App Chains</h3>
+                        <p>Extend your chain functionality with powerful Analytics and reliable APIs.</p>
+                    </div>
+                    <div className="block">
+                        <h3>Wallets</h3>
+                        <p>Forget about managing your own infrastructure and focus on building a great wallet experience.</p>
+                    </div>
+                    <div className="block">
+                        <h3>Validators</h3>
+                        <p>Measure delegator loyalty and build tools to engage with them.</p>
+                    </div>
+                    <div className="block">
+                        <h3>Analysts</h3>
+                        <p>Dive deep into Cosmos on-chain data and run complex analysis.</p>
+                    </div>
+                    <div className="block">
+                        <h3>MEV</h3>
+                        <p>Access on-chain data and necessary infrastructure.</p>
+                    </div>
+                    <div className="block">
+                        <h3>Smart Contracts</h3>
+                        <p>Obtain on-chain data and get custom endpoints for your smart contract.</p>
+                    </div>
+                    <div className="block">
+                        <h3>Defi Apps</h3>
+                        <p>Forget about complex RPC queries and request all data from one single platform.</p>
+                    </div>
+                    <div className="block">
+                        <h3>IBC Apps</h3>
+                        <p>The most comprehensive data set for IBC chains and scalable IBC infrastructure.</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
